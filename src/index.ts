9877a06ed8ff70f23ecd354feb29aa0ae59304fc
Max Zakharzhevskiy <maxzz@msn.com>
2021-04-12 19:42:22 -0700

    WIP: tested as a good START POINT for new projects

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,57 @@ import { exitProcess, help, newErrorArgs, notes } from './process-utils';
 
 namespace osStuff {
 
+    export type fileItem = {
+        short: string;      // filename wo/ path
+        btime: Date;        // file created (birthtime) timestamp
+        mtime?: Date;       // file data modified timestamp; present if different from btime
+        size: number;       // file size
+    }
+
+    export type folderItem = {
+        name: string;       // Folder full name
+        files: fileItem[];  // Short filenames i.e. wo/ path.
+        subs: folderItem[]; // Sub-folders.
+    }
+
+    function collectFiles(dir: string, rv: folderItem, recursive: boolean): void {
+        rv.files.push(...fs.readdirSync(dir).map((_) => {
+            let fname = path.join(dir, _);
+            let st = fs.statSync(fname);
+            if (st.isDirectory()) {
+                if (recursive) {
+                    let newFolder: folderItem = {
+                        name: fname,
+                        files: [],
+                        subs: [],
+                    };
+                    collectFiles(fname, newFolder, recursive);
+                    if (newFolder.files.length || newFolder.subs.length) {
+                        rv.subs.push(newFolder);
+                    }
+                }
+            } else if (st.isFile()) {
+                let newFile: fileItem = {
+                    short: _,
+                    btime: st.birthtime,
+                    ...(st.birthtime !== st.mtime && {mtime: st.mtime}),
+                    size: st.size,
+                };
+                return newFile;
+            }
+        }).filter(Boolean));
+    }
+
+    export function collectDirItems(dir: string): folderItem {
+        let rv: folderItem = {
+            name: dir,
+            files: [],
+            subs: [],
+        };
+        collectFiles(dir, rv, true);
+        return rv;
+    }
+
 } //namespace osStuff
 
 namespace appUtils {
@@ -39,7 +90,7 @@ namespace appUtils {
 
 function handleFiles(filesToRar: string[]): void {
     // 0. Simulate rardir behaviour. Files should be in the same folder.
-
+/*
     let root = path.dirname(filesToRar[0]);
     let files = filesToRar.map(_ => path.basename(_));
     let fnameRar = path.join(root, 'tm.rar');
@@ -52,6 +103,7 @@ function handleFiles(filesToRar: string[]): void {
     // Create dirs.txt and add to tm.rar.
 
     appUtils.createFileMp4WithSrt(fnameRar, root, files);
+*/    
 }
 
 function handleFolder(dir: string) {
@@ -87,31 +139,37 @@ async function main() {
     let args = require('minimist')(process.argv.slice(2), {
     });
     
-    console.log(`args ${JSON.stringify(args, null, 4)}`);
+    //console.log(`args ${JSON.stringify(args, null, 4)}`);
     //await exitProcess(0, '');
 
     let targets = checkArg(args._ || []);
-/*
+
     // If we have a single top folder and no top files w/ drag&drop then check what we have inside.
     if (targets.dirs.length === 1 && !targets.files.length) {
         let target = targets.dirs[0];
 
-        if (path.basename(target).toLowerCase() === 'tm') {
-            targets.files = fs.readdirSync(target).map(_ => path.join(target, _));
-            targets.dirs = [];
+        let root: osStuff.folderItem = osStuff.collectDirItems(target);
+        if (root.files.length) {
+            // This is not an error, just a regular case.
+            //notes.add(`--- INFO: Skipped mixed content (folder(s) and file(s) in:)\n    b:${root.name}`);
         } else {
-            let root: osStuff.folderItem = osStuff.collectDirItems(target);
-            if (root.files.length) {
-                // This is not an error, just a regular case.
-                //notes.add(`--- INFO: Skipped mixed content (folder(s) and file(s) in:)\n    b:${root.name}`);
-            } else {
-                targets.dirs = root.subs.map((_: osStuff.folderItem) => _.name);
-            }
+            targets.dirs = root.subs.map((_: osStuff.folderItem) => _.name);
         }
     }
 
-    // console.log(`targets ${JSON.stringify(targets, null, 4)}`);
-    // await exitProcess(0, '');
+    console.log(`targets ${JSON.stringify(targets, null, 4)}`);
+    await exitProcess(0, '');
+    /*
+        targets {
+            "files": [
+                "C:\\Y\\w\\1-node\\1-utils\\merge-video-subtitles\\tsconfig.json"
+            ],
+            "dirs": [
+                "C:\\Y\\w\\1-node\\1-utils\\merge-video-subtitles\\tests",
+                "C:\\Y\\w\\1-node\\1-utils\\merge-video-subtitles\\src"
+            ]
+        }    
+    */
 
     if (targets.files.length) {
         handleFiles([...targets.files, ...targets.dirs]); // TOOO: Check: all files and folders should be inside the same folder (although it isn't possible with drag&drop).
@@ -120,9 +178,9 @@ async function main() {
             handleFolder(dir);
         }
     } else {
-        throw newErrorArgs(`Specify at leats one folder or files name to process.`);
+        throw newErrorArgs(`Specify at leats one folder or files name to process`);
     }
-*/
+
     notes.show();
 }
 
