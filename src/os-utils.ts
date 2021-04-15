3e5b50217adb5a35cd62cb546f2bcfbf8421b11e
Max Zakharzhevskiy <maxzz@msn.com>
2021-04-14 17:51:24 -0700

    WIP: remove indent tests

--- src/os-utils.ts
+++ src/os-utils.ts
@@ -73,3 +73,42 @@ export namespace fnames {
     }
     
 } //namespace fnames
+
+export function removeIndent(src: string, all?: boolean): string {
+    // 1. if defined all then remove all indentation from each line
+    if (all) {
+        return src.replace(/^[^\S\n]+/gm, '');
+    }
+    // 2. remove the shortest leading indentation from each line
+    const match = src.match(/^[^\S\n]*(?=\S)/gm);
+    const indent = match && Math.min(...match.map(el => el.length));
+    if (indent) {
+        const regexp = new RegExp(`^.{${indent}}`, 'gm');
+        return src.replace(regexp, '');
+    }
+    return src;
+}
+
+// helpers for back quatas strings
+
+function caclLineIndent(line: string): number {
+    const whitespace = /^(\s+)/;
+    let space = whitespace.exec(line);
+    return +(space && space[1].length);
+} //caclLineIndent()
+
+export function reduceIndentByLast(s: string): string {
+    // 0. Reduce the indent of multi-line string by the last line indent.
+    let arr = s.split(/\r?\n/g);
+    if (arr.length < 2) {
+        return s;
+    }
+    let last = caclLineIndent(arr[arr.length-1]);
+    arr.forEach((line: string, index: number) => {
+        let l = caclLineIndent(line);
+        if (l >= last) {
+            arr[index] = line.slice(last);
+        }
+    });
+    return arr.join('\n');
+} //reduceIndentByLast()
