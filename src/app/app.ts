b1cf44a133981aec6fa9cb2105a0f440053e6808
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-06 13:57:17 -0800

    WIP. srt. make uniform backup extension

--- src/app/app.ts
+++ src/app/app.ts
@@ -98,7 +98,7 @@ function checkSubtitlesFormat(fullFname: string, options: AppOptions) {
 
     function createBackup(fname: string) {
         if (options.keepOrg) {
-            const backupName = replaceExt(fname, `._${path.extname(fname).substring(2) || ''}`);
+            const backupName =  replaceExt(fname, '.___'); // replaceExt(fname, `._${path.extname(fname).substring(2) || ''}`);
             if (!exist(backupName)) {
                 const cnt = fs.readFileSync(fullFname, { encoding: 'utf-8' });
                 fs.writeFileSync(backupName, cnt);
