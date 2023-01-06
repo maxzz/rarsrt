310597ee4c95991c7e088ac01d79777a00ce65ac
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-05 18:39:01 -0800

    WIP. srt. add removeSrtDoubleCounters()

--- src/utils/utils-vtt.ts
+++ src/utils/utils-vtt.ts
@@ -120,29 +120,79 @@ function fixVttLine(line: string, context: Context): string {
     return vttLine;
 }
 
-/*
-Bad format (lines 2 and 6 should not be in vtt):
-0:'WEBVTT'
-1:''
-2:'0'
-3:'00:00:05.381 --> 00:00:10.340'
-4:'In this lesson, we will look at the main string data type in Unreal Engine - FString,'
-5:''
-6:'1'
-7:'00:00:10.340 --> 00:00:15.000'
-8:'but before we start, let's create our own logging category.'
-9:''
-length:10
-*/
 function removeVttCounters(lines: string[], context: Context): string[] {
+    /*
+    Bad format (lines 2 and 6 should not be in vtt):
+        0:'WEBVTT'
+        1:''
+        2:'0'
+        3:'00:00:05.381 --> 00:00:10.340'
+        4:'In this lesson, we will look at the main string data type in Unreal Engine - FString,'
+        5:''
+        6:'1'
+        7:'00:00:10.340 --> 00:00:15.000'
+        8:'but before we start, let's create our own logging category.'
+        9:''
+        length:10
+    */
     const types: LineMeaning[] = getLinesMeaning(lines);
 
-    const newLines = types.map((type, idx) => {
-        const isCounter = type.type === LineType.counter
-            && types[idx + 1].type === LineType.stamp;
-        isCounter && (context.hasFixes = true);
-        return isCounter ? undefined : type;
-    }).filter(Boolean).map((type) => type.line);
+    const newLines = types
+        .map((type, idx) => {
+            const isCounter = type.type === LineType.counter
+                && types[idx + 1].type === LineType.stamp;
+            isCounter && (context.hasFixes = true);
+            return isCounter ? undefined : type;
+        })
+        .filter(Boolean)
+        .map((type) => type.line);
+
+    return newLines;
+}
+
+function removeSrtDoubleCounters(lines: string[], context: Context): string[] {
+    /*
+    Bad format (lines 0 and 4 should not be in srt):
+        0:'0'
+        1:'1'
+        2:'00:00:01,280 --> 00:00:08,450'
+        3:'Hello and welcome to this section on prepositions. Prepositions are these kind of small words that'
+        4:'1'
+        5:'2'
+        6:'00:00:08,450 --> 00:00:16,640'
+        7:'go before nouns and show the relationship between the noun and the rest of the sentence. There are'
+    */
+    /*
+    Bad format (between line 4 and 6 is empty line):
+        0:{type: 0, line: '0'}
+        1:{type: 0, line: '1'}
+        2:{type: 1, line: '00:00:01,280 --> 00:00:08,450'}
+        3:{type: 3, line: 'Hello and welcome to this section on prepositi…epositions are these kind of small words that'}
+        4:{type: 0, line: '1'}
+        5:{type: 2, line: ''}
+        6:{type: 0, line: '2'}
+        7:{type: 1, line: '00:00:08,450 --> 00:00:16,640'}
+        8:{type: 3, line: 'go before nouns and show the relationship betw… noun and the rest of the sentence. There are'}
+    */
+    const types: LineMeaning[] = getLinesMeaning(lines);
+
+    const newLines = types
+        .map((type, idx) => {
+            const isDoubleCounter =
+                type.type === LineType.counter &&
+                (
+                    types[idx + 1].type === LineType.counter &&
+                    types[idx + 2].type === LineType.stamp
+                ) || (
+                    types[idx + 1].type === LineType.empty &&
+                    types[idx + 2].type === LineType.counter &&
+                    types[idx + 3].type === LineType.stamp
+                );
+            isDoubleCounter && (context.hasFixes = true);
+            return isDoubleCounter ? undefined : type;
+        })
+        .filter(Boolean)
+        .map((type) => type.line);
 
     return newLines;
 }
@@ -165,50 +215,6 @@ export function convertVttToSrt(fileContent: string, action: ConvertAction): Con
     };
 }
 
-/*
-Bad format (lines 0 and 4 should not be in srt):
-0:'0'
-1:'1'
-2:'00:00:01,280 --> 00:00:08,450'
-3:'Hello and welcome to this section on prepositions. Prepositions are these kind of small words that'
-4:'1'
-5:'2'
-6:'00:00:08,450 --> 00:00:16,640'
-7:'go before nouns and show the relationship between the noun and the rest of the sentence. There are'
-*/
-/*
-Bad format (between line 4 and 6 is empty line):
-0:{type: 0, line: '0'}
-1:{type: 0, line: '1'}
-2:{type: 1, line: '00:00:01,280 --> 00:00:08,450'}
-3:{type: 3, line: 'Hello and welcome to this section on prepositi…epositions are these kind of small words that'}
-4:{type: 0, line: '1'}
-5:{type: 2, line: ''}
-6:{type: 0, line: '2'}
-7:{type: 1, line: '00:00:08,450 --> 00:00:16,640'}
-8:{type: 3, line: 'go before nouns and show the relationship betw… noun and the rest of the sentence. There are'}
-*/
-function removeSrtCounters(lines: string[], context: Context): string[] {
-    const types: LineMeaning[] = getLinesMeaning(lines);
-
-    const newLines = types.map((type, idx) => {
-        const isDoubleCounter =
-            type.type === LineType.counter &&
-            (
-                types[idx + 1].type === LineType.counter &&
-                types[idx + 2].type === LineType.stamp
-            ) || (
-                types[idx + 1].type === LineType.empty &&
-                types[idx + 2].type === LineType.counter &&
-                types[idx + 3].type === LineType.stamp
-            );
-        isDoubleCounter && (context.hasFixes = true);
-        return isDoubleCounter ? undefined : type;
-    }).filter(Boolean).map((type) => type.line);
-
-    return newLines;
-}
-
 export function fixSrt(fileContent: string): ConvertResult {
     const context: Context = {
         ccCount: 0,
@@ -216,7 +222,7 @@ export function fixSrt(fileContent: string): ConvertResult {
         action: ConvertAction.fix,
     };
 
-    const lines = removeSrtCounters(fileContent.split(/\r?\n/), context);
+    const lines = removeSrtDoubleCounters(fileContent.split(/\r?\n/), context);
     const newContent = lines.map((line) => fixVttLine(line, context)).filter((line) => line !== undefined).join(EOL);
 
     return {
